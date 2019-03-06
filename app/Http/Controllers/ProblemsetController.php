be68b19877420a18281fb6c072808dbd4517dd47
wearrys <1721604476@qq.com>
2019-03-06 11:44:20 +0800

    support markdown and mathjax

--- app/Http/Controllers/ProblemsetController.php
+++ app/Http/Controllers/ProblemsetController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\Markdowner;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -12,18 +13,13 @@ class ProblemsetController extends Controller
         $problemset = DB::table('problemset') -> paginate(20);
         return view('problemset.list', ['problemset' => $problemset]);
     }
-
-    public function fetch() 
-    {
-        $problemset = DB::table('problemset') -> paginate(20);
-        dd ($problemset);
-    }
-
+    
     public function showProblem($id)
     {
-        $content = DB::table('problemset') -> where('id' => $id);
-
-        md -> <html></html>
-        return view('problemset.show', ['contend_html', $contend_html]);
+        $markdowner = new Markdowner();
+        $content = DB::table('problemset') -> where('id', $id) -> first();
+        $title = $content -> title;
+        $content_html = $markdowner -> toHTML($content -> content_md);
+        return view('problemset.show', ['title' => $title, 'content_html' => $content_html]);
     }
 }
