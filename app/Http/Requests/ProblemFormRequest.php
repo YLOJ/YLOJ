25066873ff721617558a00b8be0aab99520e2c67
demerzel-iv <demerzel_iv@qq.com>
2019-03-07 10:49:06 +0800

    nothing

--- app/Http/Requests/ProblemFormRequest.php
+++ app/Http/Requests/ProblemFormRequest.php
@@ -24,13 +24,13 @@ class SubmitFormRequest extends FormRequest
     public function rules()
     {
         return [
-            'title' => 'required|string|max:255',
+            'title' => 'bail|required|string|max:255',
             'content_md' => 'required|string|max:65000'
             //
         ];
     }
 
-    public function message() {
+    public function messages() {
         return [
             'title.required' => "Title can't be empty",
             'title.max' => "Title is too long",
--- resources/views/problemset/add.blade.php
+++ resources/views/problemset/add.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 
 <form method="post" action="/problem/add_submit">
-title : <input type="text" name="title"> <br>
-content(markdown) : <br> <textarea rows="20" cols="60" name="content_md">
-</textarea><br>
-<button type="submit">submit</button>
-
+    title : <input type="text" name="title"> <br>
+    content(markdown) : <br> <textarea rows="20" cols="60" name="content_md">
+    </textarea><br>
+    <button type="submit">submit</button>
+    @csrf
 </form>
 
 @endsection
