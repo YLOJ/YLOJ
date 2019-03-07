a10f54c43936f8aaa5d402d65c915139bb2dbda9
demerzel-iv <demerzel_iv@qq.com>
2019-03-07 15:32:36 +0800

    edit view

---
+++ resources/views/problemset/edit.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+    <form action="/problem/edit_submit/{{$id}}" method="post">    
+        <div class="form-group">
+            <label>Title</label>
+            <input type="text" name="title" class="form-control" value="{{$title}}">
+        </div>
+        <div class="form-group">
+            <label>Content</label> <br>
+            <textarea rows="20" name="content_md", class="form-control">
+                {{$content_md}}
+            </textarea>
+        </div>
+        <button type="submit" class="btn btn-primary"> Add </button>
+        @csrf
+    </form>
+</div>
+@endsection
