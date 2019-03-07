17acef54f2d59fa2956e563121b9c4d043655015
dy0607 <dy0607@foxmail.com>
2019-03-07 16:57:12 +0800

    show problem

--- resources/views/layouts/problem.blade.php
+++ resources/views/layouts/problem.blade.php
@@ -2,30 +2,36 @@
 
 @section("content")
 
-<div class="artcle">
-    <h1> @yield("title") </h1>
+<div class="container">
+	<div class="row">
+		<div class="col">
+			<h1><center> @yield("title") </h1> <br>
+			Time Limit : {{ $time_limit }} ms <br>
+			Memory Limit : {{ $memory_limit }} M <br>
+			
+			<div class = "btn-group-md">
+				<button class = "btn btn-primary" href="#"> submit </button>
+				<button class = "btn btn-primary" href="#"> submissions </button>
+				<button class = "btn btn-primary" href="#"> dicussions </button>
+				<button class = "btn btn-primary" href="#"> statistics </button>
+				<button class = "btn btn-primary" href="#"> custom tests </button>
 
-    <h5><nav>
-        <a href="#">submit</a> |
-        <a href="#">submissions</a> |
-        <a href="#">discussions</a> |
-        <a href="#">statistics</a> |
-        <a href="#">custom test</a>
 
-        @auth
-            @if ( Auth::user()->permission > 0)
-            | <a href="javascript:void(0);" onclick="document.getElementById('myform').submit();">edit</a>
-            <form id="myform" method="post" action="/problem/edit/{{$id}}">
-                @csrf
-            </form>
-            @endif
-        @endauth
+				@auth
+					@if ( Auth::user()->permission > 0)
+					<button class = "btn btn-primary" href="javascript:void(0);" onclick="document.getElementById('myform').submit();"> edit </button>
+					<form id="myform" method="post" action="/problem/edit/{{$id}}">
+						@csrf
+					</form>
+					@endif
+				@endauth
+			</div>
+			</center> <br>
 
-    </nav></h5>
-
-    <hr>
-
-    @yield("problem_content")
+			@yield("problem_content")
+		
+		</div>
+	</div>
 </div>
 
 @endsection
