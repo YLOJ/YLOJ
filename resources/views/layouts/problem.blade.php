08078dc90ab603ebea9e19415a324783540603f1
dy0607 <dy0607@foxmail.com>
2019-03-07 16:57:12 +0800

    show problem

--- resources/views/layouts/problem.blade.php
+++ resources/views/layouts/problem.blade.php
@@ -2,34 +2,41 @@
 
 @section("content")
 
-<div class="artcle">
-    <h1> @yield("title") </h1>
+<div class="container">
+	<div class="row">
+		<div class="col">
+			<h1><center> @yield("title") </h1>
+			<center> Time Limit : {{ $time_limit }} ms </center>
+			<center> Memory Limit : {{ $memory_limit }} M </center> <br>
 
-    <h3>
-        <nav>
-            <a href="#">submit</a> |
-            <a href="#">submissions</a> |
-            <a href="#">discussions</a> |
-            <a href="#">statistics</a> |
-            <a href="#">custom test</a>
+			<center>
+			<div class = "btn-group-md">
+				<button class = "btn btn-primary" href="#"> submit </button>
+				<button class = "btn btn-primary" href="#"> submissions </button>
+				<button class = "btn btn-primary" href="#"> dicussions </button>
+				<button class = "btn btn-primary" href="#"> statistics </button>
+				<button class = "btn btn-primary" href="#"> custom tests </button>
 
-            @auth
-            @if ( Auth::user()->permission > 0)
-            |<a href="javascript:void(0);" onclick="document.getElementById('myform').submit();">edit</a>
-            <form id="myform" method="post" action="/problem/edit/{{$id}}">
-                @csrf
-            </form>
-            @endif
-            @endauth
 
-        </nav>
-    </h3>
+				@auth
+					@if ( Auth::user()->permission > 0)
+					<button class = "btn btn-primary" href="javascript:void(0);" onclick="document.getElementById('myform').submit();"> edit </button>
+					<form id="myform" method="post" action="/problem/edit/{{$id}}">
+						@csrf
+					</form>
+					@endif
+				@endauth
+			</div>
+			</center> <br>
 
-    <hr>
+			@yield("problem_content")
+		
+		</div>
+	</div>
+<<<<<<< HEAD
+=======
 
-    <h4>
-        @yield("problem_content")
-    </h4>
+>>>>>>> d4a4e38... fix
 </div>
 
 @endsection
