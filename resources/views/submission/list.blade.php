c26aadb9548b0510085ddb626216acbb773546a6
dy0607 <dy0607@foxmail.com>
2019-04-01 11:25:05 +0800

    submission list

--- resources/views/submission/list.blade.php
+++ resources/views/submission/list.blade.php
@@ -5,32 +5,58 @@
 
 <div class="container">
 
+	<div class="row justify-content-center">
+        {{ $submission -> links() }}
+    </div>
+
     <div class="row">
         <div class="col">
             <table class="table table-bordered">
                 <thead>
                     <tr>
-                        <th style="width:150px">Problem ID</th>
-                        <th style="width:900px">Title</th>
-                        <th style="width:100px">Rating</th>
+                        <th style="width:12%">Submission ID</th>
+						<th style="width:25%">Problem Name</th>
+						<th style="width:10%">User</th>
+						<th style="width:9%">Result</th>
+						<th style="width:9%">Score</th>
+						<th style="width:9%">Time</th>
+						<th style="width:9%">Memory</th>
+						<th style="width:17%">Submission Time</th>
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($problemset as $problem)
-                    @if ($problem -> id % 2 == 1) <tr style="background-color:#F3F3F3">
-                        @else
-                    <tr>
-                        @endif
-                        <td> {{ $problem -> id }} </td>
+                    @foreach ($submissionset as $sub)
+					@if ($sub -> id % 2 == 1) 
+						<tr style="background-color:#F3F3F3">
+                    @else
+						<tr>
+					@endif
+						<td> {{ $sub -> id }} </td>
+						<td>
+							<a href="/problem/{{ $sub -> problem_id }}"> #{{ $sub -> problem_id }} : {{ $sub -> problem_name }} </a>
+						</td>
+						<td> {{ $sub -> user_name }} </td>
+						<td> 
+						@if ($sub -> result == "accepted") <a class="text-success">
+						@else <a class="text-danger">
+						@endif
+						<b>	{{ $sub -> result }} </b> </a>
+						</td>
+						<td> 
+						@if ($sub -> score == 100) <a class="text-success">
+						@elseif ($sub -> score > 0) <a class="text-warning">
+						@else <a class="text-danger">
+						@endif
+						<b> <a href="/submission/{{$sub -> id}}"> {{ $sub -> score }} </a> </b> </a>
+						</td>
+						<td> {{ $sub -> time_used }}ms </td>
+						<td> {{ $sub -> memory_used }}kb </td>
+						<td> {{ $sub -> created_at }} </td>
                     </tr>
                     @endforeach
                 </tbody>
             </table>
         </div>
-    </div>
-
-    <div class="row justify-content-center">
-        {{ $problemset -> links() }}
     </div>
 
 </div>
