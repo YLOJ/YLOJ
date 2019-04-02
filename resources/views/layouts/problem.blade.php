70e57069d5f78c0ac2e2858452fa1b8b12c824da
wearrys <1721604476@qq.com>
2019-04-02 15:55:32 +0800

    fix-up

--- resources/views/layouts/problem.blade.php
+++ resources/views/layouts/problem.blade.php
@@ -3,42 +3,44 @@
 @section("content")
 
 <div class="container">
-	<div class="row">
-		<div class="col">
-			<h1><center> @yield("title") </h1>
-			<center> Time Limit : {{ $time_limit }} ms </center>
-			<center> Memory Limit : {{ $memory_limit }} M </center> <br>
+    <div class="row">
+        <div class="col">
+            <div class="text-center">
+                <h1> @yield("title") </h1>
+                Time Limit : {{ $time_limit }} Ms <br>
+                Memory Limit : {{ $memory_limit }} Mb <br> <br>
 
-			<center>
-			<div class = "btn-group-md">
-				<button class = "btn btn-primary" href="#"> 
-					<img src="{{ asset('svg/icons/paper-plane.ico') }}"/> Submit </button>
-				<button class = "btn btn-primary" href="#"> 
-					<img src="{{ asset('svg/icons/text-left.ico') }}"/> Submissions </button>
-				<button class = "btn btn-primary" href="#"> 
-					<img src="{{ asset('svg/icons/discussion.ico') }}"/> Dicussions </button>
-				<button class = "btn btn-primary" href="#"> 
-					<img src="{{ asset('svg/icons/statistics.ico') }}"/> Statistics </button>
-				<button class = "btn btn-primary" href="#"> 
-					<img src="{{ asset('svg/icons/test-file.ico') }}"/> Custom tests </button>
+                <div class="btn-group-md">
+                    <button class="btn btn-sm btn-primary" href="#">
+                        <img src="{{ asset('svg/icons/paper-plane.ico') }}" /> Submit </button>
+                    <button class="btn btn-sm btn-primary" href="#">
+                        <img src="{{ asset('svg/icons/text-left.ico') }}" /> Submissions </button>
+                    <button class="btn btn-sm btn-primary" href="#">
+                        <img src="{{ asset('svg/icons/discussion.ico') }}" /> Dicussions </button>
+                    <button class="btn btn-sm btn-primary" href="#">
+                        <img src="{{ asset('svg/icons/statistics.ico') }}" /> Statistics </button>
+                    <button class="btn btn-sm btn-primary" href="#">
+                        <img src="{{ asset('svg/icons/test-file.ico') }}" /> Custom tests </button>
 
 
-				@auth
-					@if ( Auth::user()->permission > 0)
-					<button class = "btn btn-danger" href="javascript:void(0);" onclick="document.getElementById('myform').submit();"> 
-						<img src="{{ asset('svg/icons/edit.ico') }}"/> Edit </button>
-					<form id="myform" method="post" action="/problem/edit/{{$id}}">
-						@csrf
-					</form>
-					@endif
-				@endauth
-			</div>
-			</center> <br>
+                    @auth
+                    @if ( Auth::user()->permission > 0)
+                    <button class="btn btn-sm btn-danger" href="javascript:void(0);" onclick="document.getElementById('myform').submit();">
+                        <img src="{{ asset('svg/icons/edit.ico') }}" /> Edit </button>
+                    <form id="myform" method="post" action="/problem/edit/{{$id}}">
+                        @csrf
+                    </form>
+                    @endif
+                    @endauth
+                </div>
+            </div>
 
-			@yield("problem_content")
-		
-		</div>
-	</div>
+            <br>
+
+            @yield("problem_content")
+
+        </div>
+    </div>
 </div>
 
-@endsection
+@endsection 
