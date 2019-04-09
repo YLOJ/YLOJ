adf9658c5ccbb336ae4e563ffd4623c42602a01b
demerzel-iv <demerzel_iv@qq.com>
2019-04-09 14:58:40 +0800

    delete layouts.problem fix problem.show.submission

--- resources/views/problemset/show.blade.php
+++ resources/views/problemset/show.blade.php
@@ -1,9 +1,61 @@
-@extends("layouts.problem")
+@extends("layouts.app")
 
-@section("title")
-{{ $title }}
-@endsection
+@section("content")
 
-@section("problem_content")
-<?php echo $content_html ?>
-@endsection
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="text-center">
+                <h1> {{ $title }} </h1>
+                Time Limit : {{ $time_limit }} Ms <br>
+                Memory Limit : {{ $memory_limit }} Mb <br> <br>
+
+                <div class="btn-group-md">
+					<a href="{{ url('/problem/submit/'.$id) }}">
+						<button class="btn btn-sm btn-primary" >
+							<img src="{{ asset('svg/icons/paper-plane.ico') }}" /> Submit 
+						</button>
+					</a>
+					<a href="{{ url('/submission?problem_id='.$id) }}">
+						<button class="btn btn-sm btn-primary">
+							<img src="{{ asset('svg/icons/text-left.ico') }}" /> Submissions 
+						</button>
+					</a>
+					<a href="#">
+						<button class="btn btn-sm btn-primary">
+							<img src="{{ asset('svg/icons/discussion.ico') }}" /> Dicussions 
+						</button>
+					</a>
+					<a href="#">
+						<button class="btn btn-sm btn-primary">
+							<img src="{{ asset('svg/icons/statistics.ico') }}" /> Statistics 
+						</button>
+					</a>
+					<a href="{{ url('/problem/customtests') }}">
+						<button class="btn btn-sm btn-primary">
+							<img src="{{ asset('svg/icons/test-file.ico') }}" /> Custom tests 
+						</button>
+					</a>
+
+
+                    @auth
+                    @if ( Auth::user()->permission > 0)
+                    <button class="btn btn-sm btn-danger" href="javascript:void(0);" onclick="document.getElementById('myform').submit();">
+                        <img src="{{ asset('svg/icons/edit.ico') }}" /> Edit </button>
+                    	<form id="myform" method="post" action="/problem/edit/{{$id}}">
+                        @csrf
+                    </form>
+                    @endif
+                    @endauth
+                </div>
+            </div>
+
+            <br>
+
+            <?php echo $content_html ?>
+
+        </div>
+    </div>
+</div>
+
+@endsection 
--- resources/views/submission/customtests.blade.php
+++ resources/views/submission/customtests.blade.php
@@ -5,11 +5,11 @@
     <form action="#" method="post">
         <div class="form-group">
             <label>Source Code</label> <br>
-            <textarea rows="15" name="source_code", class="form-control"></textarea>
+            <textarea rows="15" name="source_code" , class="form-control"></textarea>
         </div>
-	<div class="form-group">
+        <div class="form-group">
             <label>Input Text File</label> <br>
-            <textarea rows="5" name="source_code", class="form-control"></textarea>
+            <textarea rows="5" name="source_code" , class="form-control"></textarea>
         </div>
         <button type="submit" class="btn btn-primary"> Submit </button>
         @csrf
