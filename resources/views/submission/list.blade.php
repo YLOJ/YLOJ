b2a291201c36340297fd734ab7a89bdff2f5109a
demerzel-iv <demerzel_iv@qq.com>
2019-04-10 16:32:01 +0800

    waitin submission display

--- resources/views/submission/list.blade.php
+++ resources/views/submission/list.blade.php
@@ -23,40 +23,40 @@ else $max_score = "";
 
 <div class="container">
 
-	<p class="text-sm"> </p>
-	<div class="row">
-		<div class="col-md-10">
-			<div class="hidden-xs">
-				<form class="form-inline" action="/submission" method="get">
-					<div class="form-group">
-						<label class="control-label"> &nbsp Problem ID: &nbsp </label>
-						<input class="form-control" type="text" name="problem_id" style="height:2em;width:4em" value={{$problem_id}}>
-					</div>
+    <p class="text-sm"> </p>
+    <div class="row">
+        <div class="col-md-10">
+            <div class="hidden-xs">
+                <form class="form-inline" action="/submission" method="get">
+                    <div class="form-group">
+                        <label class="control-label"> &nbsp Problem ID: &nbsp </label>
+                        <input class="form-control" type="text" name="problem_id" style="height:2em;width:4em" value={{$problem_id}}>
+                    </div>
 
-					<div class="form-group">
-						<label class="control-label"> &nbsp&nbsp User Name: &nbsp </label>
-						<input class="form-control" type="text" name="user_name" style="height:2em;width:10em" value={{$user_name}}>
-					</div>
+                    <div class="form-group">
+                        <label class="control-label"> &nbsp&nbsp User Name: &nbsp </label>
+                        <input class="form-control" type="text" name="user_name" style="height:2em;width:10em" value={{$user_name}}>
+                    </div>
 
-					<div class="form-group">
-						<label class="control-label"> &nbsp&nbsp Score: &nbsp </label>
-						<input class="form-control" type="text" name="min_score" style="height:2em;width:4em" value={{$min_score}}>
-						&nbsp ~ &nbsp
-						<input class="form-control" type="text" name="max_score" style="height:2em;width:4em" value={{$max_score}}> &nbsp&nbsp&nbsp
-					</div>
-					<button class="btn btn-primary btn-sm" type="submit">
-						<img src="{{ asset('svg/icons/search.ico') }}" class="icon-sm"/> Search </button>
-				</form>
-			</div>
-		</div>
-		<div class="col-md-2" style="text-align:right">
-		@auth
-			<a href="{{ url('/submission?user_name='.Auth::User() -> name.'&problem_id='.$problem_id.'&min_score='.$min_score.'&max_score='.$max_score) }}"> 
-				<button class="btn btn-primary btn-sm"> My Submissions </button>
-			</a>
-		@endauth
-		</div>
-	</div>
+                    <div class="form-group">
+                        <label class="control-label"> &nbsp&nbsp Score: &nbsp </label>
+                        <input class="form-control" type="text" name="min_score" style="height:2em;width:4em" value={{$min_score}}>
+                        &nbsp ~ &nbsp
+                        <input class="form-control" type="text" name="max_score" style="height:2em;width:4em" value={{$max_score}}> &nbsp&nbsp&nbsp
+                    </div>
+                    <button class="btn btn-primary btn-sm" type="submit">
+                        <img src="{{ asset('svg/icons/search.ico') }}" class="icon-sm" /> Search </button>
+                </form>
+            </div>
+        </div>
+        <div class="col-md-2" style="text-align:right">
+            @auth
+            <a href="{{ url('/submission?user_name='.Auth::User() -> name.'&problem_id='.$problem_id.'&min_score='.$min_score.'&max_score='.$max_score) }}">
+                <button class="btn btn-primary btn-sm"> My Submissions </button>
+            </a>
+            @endauth
+        </div>
+    </div>
     <p class="text-sm"> </p>
 
     <div class="row">
@@ -88,20 +88,34 @@ else $max_score = "";
                         </td>
                         <td> {{ $sub -> user_name }} </td>
                         <td>
-                            @if ($sub -> result == "accepted") <a class="text-success">
+                            @if ($sub -> result == "waiting")
+                                <a class="text-primary">
+                            @else
+                                @if ($sub -> result == "accepted") <a class="text-success">
                                 @else <a class="text-danger">
-                                    @endif
+                                @endif
+                            @endif
                                     <b> {{ $sub -> result }} </b> </a>
                         </td>
                         <td>
+                            @if ($sub -> result == "waiting")
+                                <a>/</a>
+                            @else
                             @if ($sub -> score == 100) <a class="text-success">
                                 @elseif ($sub -> score > 0) <a class="text-warning">
                                     @else <a class="text-danger">
                                         @endif
                                         <b> <a href="/submission/{{$sub -> id}}"> {{ $sub -> score }} </a> </b> </a>
+                            @endif
                         </td>
-                        <td> {{ $sub -> time_used }}ms </td>
-                        <td> {{ $sub -> memory_used }}kb </td>
+
+                        @if ($sub -> result == "waiting")
+                            <td>/</td>
+                            <td>/</td>
+                        @else
+                            <td> {{ $sub -> time_used }}ms </td>
+                            <td> {{ $sub -> memory_used }}kb </td>
+                        @endif
                         <td> {{ $sub -> created_at }} </td>
                     </tr>
                     @endforeach
@@ -116,4 +130,4 @@ else $max_score = "";
 
 </div>
 
-@endsection 
+@endsection
