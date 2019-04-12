3945c6c3e11fa517e6f0fc3bdc1e4338422aafa8
quarteer <teguo@outlook.com>
2019-04-12 16:48:37 +0800

    add judge_info into database

---
+++ database/migrations/2019_04_12_084028_add_judgeinfo_into_submission.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddJudgeinfoIntoSubmission extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('submission', function (Blueprint $table) {
+            $table->string('judge_info')->after('source_code')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('submission', function (Blueprint $table) {
+            $table->dropColumn('judge_info');
+        });
+    }
+}
