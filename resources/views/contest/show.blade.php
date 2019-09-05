58af98fbd20bb20a4f9a19edfe8f484b240ad5eb
QAQAutoMaton <lk@qaq-am.com>
2019-09-05 10:34:25 +0800

    比赛页面权限号会显示edit

--- resources/views/contest/show.blade.php
+++ resources/views/contest/show.blade.php
@@ -14,7 +14,7 @@
       @include('buttons.jump-icon' , ['href' => url('/problem/customtests/') , 'icon' => 'test-file' , 'text' => 'Custom tests'])
 
       @auth
-        @if ( Auth::user()->permission > 0)
+        @if ( Auth::user()->permission > 1)
           <button class="btn btn-sm btn-danger" href="javascript:void(0);" onclick="document.getElementById('myform').submit();">
             <img src="{{ asset('svg/icons/edit.ico') }}" class="icon-sm"/> Edit </button>
           <form id="myform" method="post" action="/contest/edit/{{$id}}">
