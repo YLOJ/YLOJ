9e8f4ff53e9df816e3ac093ff3c57d2943b05b3d
QAQAutoMaton <lk@qaq-am.com>
2019-09-23 17:20:03 +0800

    设置用户名不能为中文或带空格

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,7 +49,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255', 'unique:users'],
+            'name' => ['required', 'string', 'max:255', 'unique:users','regex:/^[0-9a-zA-Z_]{1,}$/'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
         ]);
