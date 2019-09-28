c5149babe87b73cdf862e8005f270c3aaa09d289
QAQAutoMaton <lk@qaq-am.com>
2019-09-28 15:25:27 +0800

    为了生命安全，删掉[数据删除]的照片

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,8 +5,5 @@
 	<div>
 		<h2>这个页面不存在或你没有权限访问。</h2>
 
-		<h2>但你可以膜拜雅礼中学的红太阳<strong style='color: black'>h</strong><strong style='color: red'>uhao</strong></h2>
-		
-		<img src='/hhakioi.jpg' style='height: 500px;width: 1000px'>
 	</div>
 @endsection
