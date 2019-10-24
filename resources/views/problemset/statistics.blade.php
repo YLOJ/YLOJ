2ab77a58569cc7a7c5075ba88857ed387199ba62
QAQAutoMaton <lk@qaq-am.com>
2019-10-24 19:45:58 +0800

    将最短或最快的设置写入cookies

--- resources/views/problemset/statistics.blade.php
+++ resources/views/problemset/statistics.blade.php
@@ -8,9 +8,10 @@
       <h2> Statistics<a href="{{url('/problem/'.$id)}}"> #{{$id}}: {{$title}} </a> </h2>
       <br> <br>
     </div>
-	<div class="mdui-tab" mdui-tab>
-	  <a href="#fastest" class="mdui-ripple">最快</a>
-	  <a href="#shortest" class="mdui-ripple">最短</a>
+	<div id="statistics" class="mdui-tab">
+	  <a href="#fastest" class="mdui-ripple" id="select-fastest">最快</a>
+	  <a href="#shortest" class="mdui-ripple" id="select-shortest">最短</a>
+
 	</div>
 	<div id="fastest" class="mdui-p-a-2">
 	    <div class="row">
@@ -54,6 +55,28 @@
 	      </div>
 	    </div>
 	</div>
+	<script>
+	var $$=mdui.JQ;	
+	console.log(document.cookie);
+	console.log(document.cookie.indexOf("show-shortest="));
+	var tab=new mdui.Tab('#statistics');
+	if(document.cookie.indexOf("show-shortest=")>-1)
+		tab.show('select-shortest');
+	else 
+		tab.show('select-fastest');
+
+	mdui.mutation();
+	$$("#select-fastest").on("show.mdui.tab",function (){
+		var d = new Date();
+		d.setTime(d.getTime()-1);
+		document.cookie = 'show-shortest=1;path=/;'+"expires="+d.toGMTString();
+	});
+	$$("#select-shortest").on("show.mdui.tab",function (){
+
+		document.cookie = 'show-shortest=1;path=/';
+	});
+	</script>
+
 
     <div class="row justify-content-center">
       {{ $fastest -> links() }}
