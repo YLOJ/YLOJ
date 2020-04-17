e7f3725737685b5a88cdd265b606693b305d3972
QAQAutoMaton <lk@qaq-am.com>
2020-04-17 23:10:05 +0800

    加上搜索标题功能

--- resources/views/problemset/list.blade.php
+++ resources/views/problemset/list.blade.php
@@ -2,27 +2,55 @@
 @extends('layouts.app')
 
 @section('content')
-    <div style="text-align:center"> 
-      {{ $problemset -> links() }}
+  <?php
+    if (isset($_GET['keyword']))
+      $keyword = $_GET['keyword'];
+    else $keyword = "";
+  ?>
+
+    <div class="mdui-row mdui-container mdui-center">
+        <div class="mdui-textfield  mdui-col-offset-xs-0 mdui-col-offset-sm-3 mdui-col-xs-10 mdui-col-sm-4">
+		  <input class="mdui-textfield-input" type="text" placeholder="搜索标题" id="keyword" value={{$keyword}}>
+        </div>
+        <div style="padding-top:16px;" class="mdui-col-xs-2">
+           <button class="mdui-btn mdui-btn-icon mdui-color-theme-accent mdui-btn-raised mdui-ripple" id="submit"><i class="mdui-icon material-icons">search</i></button>
+		<script>
+			$("#keyword").keyup(function(event){  
+               if(event.keyCode ==13){  
+					self.location.href="/problem?keyword="+$("#keyword").val()
+               }  
+             });
+			$("#submit").click(function(){
+				self.location.href="/problem?keyword="+$("#keyword").val()
+			});
+		</script>
+	    @auth
+	      @if ( Auth::user()->permission > 1 )
+           <a id="add-problem" class="mdui-btn mdui-btn-icon mdui-color-theme-accent mdui-btn-raised mdui-ripple" href="/problem/add"><i class="mdui-icon material-icons">add</i></a>
+<script> 
+var inst = new mdui.Tooltip('#add-problem', {
+  content: 'Add Problem'
+});
+</script>
+
+    	  @endif
+	    @endauth
+        </div>
     </div>
-
-    @auth
-      @if ( Auth::user()->permission > 1 )
-        <div>
-        @include('buttons.jump' , ['text' => 'Add Problem','href'=>'/problem/add'])
-        </div> 
-      @endif
-    @endauth
-
     <br>
-    <?php
-      $links = $problemset -> links();
-    ?>
-
     @component('includes.problem_table', ['problemset' => $problemset])
     @endcomponent 
 
-    <div style="text-align:center"> 
-      {{ $links }}
+	<div style="text-align:center">
+    <?php
+		$links = $problemset -> links();
+		if (isset($_GET['keyword'])){
+        	$arr = explode('?', $links);
+			echo implode('?keyword='.$keyword.'&',$arr);
+		}
+		else echo $links;
+    ?>
+
+
     </div>
 @endsection
